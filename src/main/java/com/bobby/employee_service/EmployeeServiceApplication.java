49c521fccbe14aa9484cf59842eddb6e994effe7
Bob Meyer <romeyer15@Bobs-MacBook-Pro.local>
2025-06-27 11:15:24 -0400

    enable service discovery through eureka and openfeign

--- src/main/java/com/bobby/employee_service/EmployeeServiceApplication.java
+++ src/main/java/com/bobby/employee_service/EmployeeServiceApplication.java
@@ -2,10 +2,11 @@ package com.bobby.employee_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 public class EmployeeServiceApplication {
 
